e08f67fe1eb89bbf284adb348e78f22dd8a0f912
Luiz Paulo Vieira <pilovieira@gmail.com>
2025-01-28 13:49:28 -0300

    redis

--- main.js
+++ main.js
@@ -2,15 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
 const path = require('path');
-const redis = require("redis");
+const Redis = require("ioredis");
 
-const redisCli = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASS
-});
-redisCli.on("error", (err) => console.log(err));
-redisCli.connect();
+const redis = new Redis(`rediss://default:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`);
 
 const app = express();
 
@@ -27,7 +21,7 @@ const create = (req, res) => {
   try{
     const key = parseInt(Date.now() / 1000).toString(36);
     console.log(`creating '${key}' to '${req.body.url}'`);
-    redisCli.set(key, req.body.url);
+    redis.set(key, req.body.url);
     res.status(201).json({key});
   } catch(e){
     console.log(`create failed: ${e.message}`);
@@ -38,7 +32,7 @@ const create = (req, res) => {
 const redirect = (req, res) => {
   const key = req.url.replace('/', '');
   if(key){
-    redisCli.get(key, (err, url) => {
+    redis.get(key, (err, url) => {
       if(!err && url){
         console.log(`loaded key: '${key}' url:'${url}'`);
         res.writeHead(302, {'Location': url});
@@ -54,7 +48,8 @@ const redirect = (req, res) => {
 
 const writeHtml = (res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.createReadStream("./public/index.html").pipe(res);
+  const file = fs.readFileSync('./public/index.html', "utf-8");
+  res.write(file);
   res.end();
 }
 
